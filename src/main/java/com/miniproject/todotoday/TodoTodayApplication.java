2dcee05de126ec17bb42f3806d83d65b40fb394b
0empty <2zeroempty@gmail.com>
2023-04-06 02:06:04 +0900

    초기 세팅

---
+++ src/main/java/com/miniproject/todotoday/TodoTodayApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.todotoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTodayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodoTodayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/miniproject/todotoday/TodoTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.todotoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTodayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
